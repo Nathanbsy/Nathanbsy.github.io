f7a725d726923d25968348ade54c7fcdcf01c53c
Nathanbsy <nathanbs1227@gmail.com>
2024-07-25 22:46:36 -0300

    Atualizacao

---
+++ navbar.js
@@ -0,0 +1,11 @@
+
+window.addEventListener("scroll", function() {
+    let nav = this.document.querySelector('#nav');
+    let imagem = this.document.querySelector('#imgnavbar');
+    
+    if (window.scrollY > 0) {
+        nav.style.background = "black";
+    } else {
+        nav.style.background = "transparent";
+    }
+});
