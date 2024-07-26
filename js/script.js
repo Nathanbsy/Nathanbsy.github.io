1d2953e54dc71f9d9a4bec04c319f44b59f3b3fc
Nathanbsy <nathanbs1227@gmail.com>
2024-07-26 00:03:49 -0300

    menu hamburge fodastico

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
+let navCor = document.getElementById("nav");
 
 let menuMobile = document.getElementById("menuHamburguer");
 
 let botao = document.getElementById("botaaao");
 
 function menuVis() {
+
     botao.classList.toggle("ativar");
     if(menuMobile.classList.contains("invisivel")) {
         menuMobile.classList.remove("invisivel");
         menuMobile.classList.add("visivel");
+        // navCor.style.background = "#151515";
     } else {
         menuMobile.classList.remove("visivel");
         menuMobile.classList.add("invisivel");
+        // navCor.style.background = "transparent";
     }
 }
