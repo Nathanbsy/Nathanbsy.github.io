82c32e8608db6a17284f512b01480c8d4a0a9bff
Nathanbsy <nathanbs1227@gmail.com>
2024-07-27 19:26:46 -0300

    versao do site pika

--- js/botao_menu.js
+++ js/botao_menu.js
@@ -11,3 +11,22 @@ function verMais(){
         btnLeiaMais.innerHTML="LEIA MAIS";
     }
 }
+
+let trilho = document.getElementById('trilho')
+let nav = document.getElementById('nav')
+let sec1 = document.getElementById('sec1')
+let body = document.querySelector('body')
+let menu = document.getElementById('section_menu')
+let reserva = document.getElementById('section_reserva')
+let local = document.getElementById('local')
+
+trilho.addEventListener('click', ()=> {
+    trilho.classList.toggle('dark')
+    nav.classList.toggle('dark')
+    sec1.classList.toggle('dark')
+    body.classList.toggle('dark')
+    menu.classList.toggle('dark')
+    local.classList.toggle('dark')
+    reserva.classList.toggle('dark')
+
+})
