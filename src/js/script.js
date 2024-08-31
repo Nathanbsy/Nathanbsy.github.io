134c169f112901fd4e3a880f5fe09ca3ff8dc786
Nathanbsy <nathanbs1227@gmail.com>
2024-08-31 13:31:38 -0300

    tela de loading;-;

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,4 @@
-
 let menuMobile = document.getElementById("menuHamburguer");
-
 let botao = document.getElementById("botaaao");
 
 function menuVis() {
@@ -13,3 +11,19 @@ function menuVis() {
         menuMobile.classList.add("invisivel");
     }
 }
+
+// Pre loader do site
+
+let  elem_preloader = document.getElementById("preloader");
+let  elem_loader = document.getElementById("loader");
+let  elem_h1 = document.getElementById("h1pre");
+let  elem_h2 = document.getElementById("h2pre");
+let  elem_bola = document.getElementById("efeito-carregamento");
+
+
+setTimeout(function() {
+    elem_preloader.classList.remove("preloader");
+    elem_h2.style.display = "none";
+    elem_h1.style.display = "none";
+    elem_bola.style.display = "none";
+}, 2800);
